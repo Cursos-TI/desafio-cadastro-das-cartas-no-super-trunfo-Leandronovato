7d3a298b692e407e6ef7a42e06e547a392658d01
Leandro <leandromarleyusa@gmail.com>
2025-03-31 23:54:43 -0300

    Create super_trunfo.c

---
+++ super_trunfo.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+
+    char estado[1], cdcarta[3], cidade[60], estado1[1], cdcarta1[3], cidade1[60];
+    int populacao, npt, populacao1, npt1;
+    float area, pib, area1, pib1;
+          
+    printf("\nDIGITE AS ESPECEFICAÇÕES ABAIXO PARA CRIAR CARTAS PARA O 'SUPER TRUNFO'\n\n");
+        printf("Digite a Cidade: \n");         
+        scanf("%s", &cidade);
+        printf("Digite o Estado (uma letra de A à H): \n");         
+        scanf("%s", &estado);
+        printf("Digite a Quantidade da População: \n");         
+        scanf("%d", &populacao);
+        printf("Digite a Área do Território: \n");         
+        scanf("%f", &area);
+        printf("Digite o PIB da População: \n");         
+        scanf("%f", &pib);
+        printf("Digite o Número de Pontos Turísticos: \n");         
+        scanf("%d", &npt);
+        
+        printf("\n\n-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-\n\n");
+                
+        printf("Digite a Cidade: \n");         
+        scanf("%s", &cidade1);
+        printf("Digite o Estado (uma letra de A à H): \n");         
+        scanf("%s", &estado1);
+        printf("Digite a Quantidade da População: \n");         
+        scanf("%d", &populacao1);
+        printf("Digite a Área do Território: \n");         
+        scanf("%f", &area1);
+        printf("Digite o PIB da População: \n");         
+        scanf("%f", &pib1);
+        printf("Digite o Número de Pontos Turísticos: \n");         
+        scanf("%d", &npt1);
+
+        printf("\n--- CARTA 1 ---\n");
+        printf("Estado: %s\n Código: %s01\n Nome da Cidade: %s\n População: %d\n Área: %f km²\n PIB: %f Bilhões em Reais\n Número de Pontos Turísticos: %d\n", estado, estado, cidade, populacao, area, pib, npt);
+        printf("\n--- CARTA 2 ---\n");
+        printf("Estado: %s\n Código: %s02\n Nome da Cidade: %s\n População: %d\n Área: %f km²\n PIB: %f Bilhões em Reais\n Número de Pontos Turísticos: %d\n", estado1, estado1, cidade1, populacao1, area1, pib1, npt1);
+            
+    return 0;
+}
